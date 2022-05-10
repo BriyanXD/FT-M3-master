184546f8ff019b5ea11971454719377594c546fd
Briyan A <briyana1997@gmail.com>
2022-05-10 15:42:30 -0400

    Jest en in process

--- 06-Testing/homework/index.js
+++ 06-Testing/homework/index.js
@@ -1,18 +1,35 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 app.use(express.json()); // for parsing application/json
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send({
-    message: 'hola',
+    message: "hola",
+  });
+});
+app.get("/test", (req, res) => {
+  res.send({
+    message: "hola",
+  });
+});
+app.post("/sum", (req, res) => {
+  res.send({
+    result: req.body.a + req.body.b,
   });
 });
 
-app.post('/product', (req, res) => {
+app.post("/product", (req, res) => {
   res.send({
-    result: req.body.a / req.body.b,
+    result: req.body.a * req.body.b,
   });
+});
+app.post("/sumArray", (req, res) => {
+  if ([...req.body.a, ...req.body.b]) {
+    res.send({
+      result: true,
+    });
+  }
 });
 
 module.exports = app; // Exportamos app para que supertest session la pueda ejecutar
