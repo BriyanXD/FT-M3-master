2a8fac89afd12e57c6dfd03d7a4abf49d197b91c
Briyan A <briyana1997@gmail.com>
2022-05-11 12:23:17 -0400

    funcion generadora

--- 07-AsyncAwait/Generators/homework/homework.js
+++ 07-AsyncAwait/Generators/homework/homework.js
@@ -1,6 +1,13 @@
-function fizzBuzzGenerator(max) {
+function* fizzBuzzGenerator(max) {
   // Tu código acá:
+  let contador = 1;
 
+  while (contador <= max) {
+    if (contador % 3 === 0 && contador % 5 === 0) yield "Fizz Buzz";
+    else if (contador % 3 === 0) yield "Fizz";
+    else if (contador % 5 === 0) yield "Buzz";
+    else yield contador;
+    contador++;
+  }
 }
-
 module.exports = fizzBuzzGenerator;
